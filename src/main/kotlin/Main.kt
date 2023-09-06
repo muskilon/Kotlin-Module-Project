d125eca688732f536ae0f010efc10f6685b3b790
Practicum <practicum@ya.ru>
2023-09-06 13:07:09 +0300

    Еще меньше повторяющегося кода

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,53 @@
 import java.util.Scanner
 
-val ARCHIVE: List<String> = listOf("Архив", "название архива", "архив")
-val NOTE: List<String> = listOf("Заметка", "название заметки", "заметку")
+val ARCHIVE: List<String> = listOf("Название архива", "название архива", "архив")
+val NOTE: List<String> = listOf("Название заметки", "название заметки", "заметку")
 val NOTE_TEXT: List<String> = listOf("Текст", "текст заметки")
 
+fun inputString(type: List<String>, archive: Archive, archiveTitle: String): Pair<String, Boolean> {
+    val scanner = Scanner(System.`in`)
+    println("Введите ${type[1]}:")
+    val result = scanner.nextLine()
+    if(result.isEmpty()){
+        println("${type[0]} не может быть пустым")
+        return Pair(result, true)
+    }
+    when (type){
+        ARCHIVE -> {
+            if (archive.archive.keys.contains(result)) {
+                println("Архив с таким названием уже есть")
+                return Pair(result, true)
+            }
+        }
+        NOTE -> {
+            if (archive.archive[archiveTitle]!!.keys.contains(result)) {
+                println("Заметка с таким названием уже есть")
+                return Pair(result, true)
+            }
+        }
+    }
+    return Pair(result, false)
+}
 class Archive(var archive: MutableMap<String, MutableMap<String, String>>) {
     fun addArchive() {
-        val archiveTitle = inputString(ARCHIVE)
-        if (this.archive.keys.contains(archiveTitle)) println("Архив с таким названием уже есть")
-        else if (archiveTitle.isEmpty()) println("В названии архива должен быть хотя бы один символ")
-        else this.archive[archiveTitle] = mutableMapOf()
+        val archiveTitle = inputString(ARCHIVE,  this, "")
+        if (archiveTitle.second) return
+        else this.archive[archiveTitle.first] = mutableMapOf()
         return
     }
 
     fun addNote(archiveTitle: String) {
-        val noteTitle = inputString(NOTE)
-        if (this.archive[archiveTitle]!!.keys.contains(noteTitle)) println("Заметка с таким названием уже есть")
-        else if (noteTitle.isEmpty()) println("В названии заметки должен быть хотя бы один символ")
+        val noteTitle = inputString(NOTE, this, archiveTitle)
+        if (noteTitle.second) return
         else {
             while (true) {
-                val noteBody = inputString(NOTE_TEXT)
-                if (noteBody.isEmpty()) println("Заметка не может быть пустой")
-                else {
-                    this.archive[archiveTitle]!![noteTitle] = noteBody
+                val noteBody = inputString(NOTE_TEXT, this, archiveTitle)
+                if (!noteBody.second){
+                    this.archive[archiveTitle]!![noteTitle.first] = noteBody.first
                     return
                 }
             }
         }
-        return
     }
 
     fun showNote(archiveTitle: String, noteTitle: String) {
@@ -39,12 +59,6 @@ class Archive(var archive: MutableMap<String, MutableMap<String, String>>) {
         val notesMenu = Menu()
         notesMenu.showMenu(NOTE, this.archive[archiveTitle]!!.keys, this, archiveTitle)
     }
-}
-
-fun inputString(type: List<String>): String {
-    val scanner = Scanner(System.`in`)
-    println("Введите ${type[1]}:")
-    return scanner.nextLine()
 }
 
 fun main() {
